585dfacbcc9a08c624e835412f3920e78e764ae8
NokoHuang <3097932076@qq.com>
2019-07-19 17:33:16 +0800

    主页报表第一步

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -8,5 +8,15 @@ class IndexController extends Controller {
         $this->display();		
     }
 
+    public function home(){
+        $this->display();
+    }
 
+    //获取未来7天的机票销售统计
+    public function getDayticket(){
+        $Tickets = M();
+        $sql = "select count('date') as 'count',substr(date,9,10) date from (orders_item a left join ticket b on a.t_id=b.tid) group by date order by date asc";
+        $list = $Tickets->query($sql);
+        echo json_encode($list) ;
+    }
 }
