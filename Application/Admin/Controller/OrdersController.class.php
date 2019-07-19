caa335d18f024166bbdd6ae3f141c462c4044b7b
AzusaEi <897447422@qq.com>
2019-07-19 17:31:24 +0800

    订单管理

--- Application/Admin/Controller/OrdersController.class.php
+++ Application/Admin/Controller/OrdersController.class.php
@@ -9,26 +9,96 @@ class OrdersController extends Controller {
         $this->display("orders");		
     }
 
+    //获取订单
     public function getOrders(){
-    	$Order = M('orders');
+        $Order = M('orders');
 
         $arr = array();
         $arr['code'] = 0;
         $arr['msg']="";
 
-        $post = $_POST['key'];
-        $oid=$post['oid'];
-        if($tid){
-            $order = $Order->where('oid = '.$oid)->select();
-            $arr['count'] = $Ticket->where('oid = '.$oid)->count();
-            $arr['data']=$order;
+        $post = $_POST['key'];//订单号
+        $ooid=$post['ooid'];
+        if($ooid){
+            $mo=M('orders as o')->join('users as u')->where('o.ooid = '.$ooid.' and o.uid = u.uid')->select();
+            // $ticket = $Ticket->where('tid = '.$tid)->select();
+            $arr['count'] = $Order->where('ooid = '.$ooid)->count();
+            $arr['data']=$mo;
         }else{
-            $order = $Order->where()->select();
+            $mo=M('orders as o')->join('users as u')->where("o.uid = u.uid")->select();
+            // $ticket = $Ticket->where()->select();
             $arr['count'] = $Order->where()->count();
-            $arr['data']=$order;
+            $arr['data']=$mo;
         }
+    	
+        echo json_encode($arr);
+    }
+    
+    //订单-机票弹窗
+    public function ticketsShow(){
+        $oid = $_GET['oid'];
+        $this->assign('oid',$oid);
+        $this->display("ticketsShow");	
+    }
+
+    //获取订单-机票
+    public function getTickets(){
+        $oid = $_GET['oid'];
+        $Orders_item = M('orders_item');
+
+        $t_id = $Orders_item->field('t_id')->where('o_id = '.$oid)->buildSql();
+        // echo json_encode($t_id);
+
+        $Ticket = M('ticket');
+        // $ticket = $Ticket->where('tid in '.$t_id)->select();
+        // echo json_encode($ticket);
+
+        $ticket = M('ticket as t')->join('company as c')->where('t.tid in '.$t_id.'and t.cid = c.id')->select();
+        $arr = array();
+        $arr['code'] = 0;
+        $arr['msg']="";
+        $arr['count'] =  M('ticket as t')->join('company as c')->where('t.tid in '.$t_id.'and t.cid = c.id')->count();
+        $arr['data']=$ticket;
         echo json_encode($arr);
     }
 
+    //订单-常用联系人弹窗
+    public function contactShow(){
+        $oid = $_GET['oid'];
+        $this->assign('oid',$oid);
+        $this->display("contactShow");	
+    }
+
+    //获取订单-常用联系人
+    public function getContact(){
+        $oid = $_GET['oid'];
+        $Orders_item = M('orders_item');
+
+        $c_id = $Orders_item->field('c_id')->where('o_id = '.$oid)->select();
+        echo json_encode($c_id);
+
+        // $Contact = M('contact');
+        // $contact = $Contact->where('cid in '.$c_id)->select();
+        // echo json_encode($contact);
+
+        // $ticket = M('ticket as t')->join('company as c')->where('t.tid in '.$t_id.'and t.cid = c.id')->select();
+    //     $arr = array();
+    //     $arr['code'] = 0;
+    //     $arr['msg']="";
+    //     $arr['count'] =  $Contact->where('cid in '.$c_id)->count();
+    //     $arr['data']=$contact;
+    //     echo json_encode($arr);
+     }
+
+
+        //删除一行
+    public function deleteOrders(){
+        $Order = M('orders');
+        $data = [];
+        $data['msg'] = '删除成功';
+        $oid = $_POST['oid'];
+        $order = $Order->delete($oid);
+        echo json_encode($data);
+    }
 
 }
