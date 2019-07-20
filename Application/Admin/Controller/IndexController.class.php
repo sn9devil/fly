442df475d7423eefc7f9f69a802c374d0ecad9c5
NokoHuang <3097932076@qq.com>
2019-07-20 12:02:41 +0800

    报表

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -20,11 +20,19 @@ class IndexController extends Controller {
         echo json_encode($list) ;
     }
 
-    //统计各出发地的数量
-    public function getStartCity(){
+    //统计各目的地的数量
+    public function getEndCity(){
         $Tickets = M();
         $sql = "select arrive ,count('arrive') as 'count' from (orders_item a left join ticket b on a.t_id=b.tid) group by arrive order by count asc";
         $list = $Tickets->query($sql);
+        echo json_encode($list);
+    }
+     //统计各出发地的数量
+     public function getStartCity(){
+        $Tickets = M();
+        $sql = "select go ,count('go') as 'count' from (orders_item a left join ticket b on a.t_id=b.tid) group by go order by count asc";
+        $list = $Tickets->query($sql);
+        // var_dump("拿到的".$list);
         echo json_encode($list);
     }
 }
